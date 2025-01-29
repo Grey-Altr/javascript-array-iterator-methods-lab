6a7e4cc7e735fc70b9a743686be3b3a137ff2be9
GreyA <grey.altaer@gmail.com>
2025-01-29 11:44:15 -0800

    comments object added to app.js

--- app.js
+++ app.js
@@ -79,3 +79,11 @@ const devs = [
   { name: "Matt", year: 1970 },
   { name: "Wes", year: 2015 },
 ];
+
+const comments = [
+  { text: "Love this!", id: 523423 },
+  { text: "Super good", id: 823423 },
+  { text: "You are the best", id: 2039842 },
+  { text: "Ramen is my fav food ever", id: 123523 },
+  { text: "Nice Nice Nice!", id: 542328 },
+];
